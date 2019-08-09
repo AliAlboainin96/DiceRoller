c727ad794113ade4de173cd3fe63aa80d86d626e
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2019-08-09 23:59:41 +0300

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='DiceRoller',
+   version='1.0',
+   description='Dice Roller Simulator writen in python for curiosity.',
+   author='Ali Alboainin',
+   packages=['DiceRoller']
+)
