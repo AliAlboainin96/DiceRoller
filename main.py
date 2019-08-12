31b035f1ca35dac9f3888dee318deab5a91a82b7
Ali <44374885+AliAlboainin96@users.noreply.github.com>
2019-08-12 20:28:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,62 @@
 import random
-import sys
+import os
+import time
 
-loop = True
+
 
 while loop == True:
 
+    # function to clear the console
+
+    def cls():
+        os.system('Cls ' if os.name =='nt' else 'clear')
+
     roles = int(input("How Many Times Do Want To ROll The Dice? "))
-     
-    for roles_dice in range(roles):
-        random_num = random.randint(1,6)
-        print(random_num)
+ 
+    # detects if input is 0 or less
+
+    if roles == 0:
+        print("0 or below are invalid input!/n")
+
+    # menu ui
+
+    cls()
+    print("Rolling...")
+    time.sleep(1)
+    cls()
+   
+    if roles == 0:
+        print("0 or below are invalid input!/n") 
     
-    repeat = input("Do You Want To Repeat? Y/N ")
-    while repeat != "y" or "n":
+    # function to generate random numbers
 
-        print("worng choise please select Y/N")
-        if repeat == "y":
-           continue
-        elif repeat == "n":
-           sys.exit
+    def test():
+        for roles_dice in range(roles):    
+            random_num = random.randint(1,6)           
+        return random_num   
     
+    for rolls_num in range(roles):
+        rolls_num += 1
 
+        roll_form = rolls_num
 
+        print("Roll {0}: {1}".format(roll_form,test()))
+ 
+    # loop       
 
+    loop = input("\nAgain? Y/N ")
 
+    if loop == "yes" or "YES" or "y" or "Y":
+        cls()
+        loop == True
+    elif loop == "n" or "NO" or "n" or  "N":
+        cls()
+        loop == False
+        break
+    else:
+        cls()
+        print("LOADING...")
+        time.sleep(10)
+        cls()
+        print("Wrong Choise next time follow the rules!")
+        break
